8a44c9fe3f2fbb34470398e3ca89351c88b568a2
Augusto Souza <augusto_souza@edu.unifil.br>
2022-06-29 17:02:03 -0300

    :sparkles: feat: adicionado connect graphcms ao projeto

--- src/app/pages/Home/index.tsx
+++ src/app/pages/Home/index.tsx
@@ -1,3 +1,69 @@
+import { gql, useMutation, useQuery } from "@apollo/client";
+const GET_TODOS_QUERY = gql`
+  query {
+    todos {
+      id
+      nome
+      descricao
+      status_todo
+    }
+  }
+`;
+
+const CREATE_TODO_MUTATION = gql`
+  mutation CreateTodo($nome: String!, $descricao: String!) {
+    createTodo(
+      data: { nome: $nome, descricao: $descricao, status_todo: undone }
+    ) {
+      id
+    }
+  }
+`;
+
+const PUBLISH_TODO_MUTATION = gql`
+  mutation PublishTodo($id: ID!) {
+    publishTodo(where: { id: $id }, to: PUBLISHED) {
+      id
+    }
+  }
+`;
+
+const UPDATE_TODO_MUTATION = gql`
+  mutation UpdateTodo(
+    $id: ID!
+    $nome: String!
+    $descricao: String!
+    $status_todo: StatusTodo!
+  ) {
+    updateTodo(
+      data: { nome: $nome, descricao: $descricao, status_todo: $status_todo }
+      where: { id: $id }
+    ) {
+      id
+    }
+  }
+`;
+
+const DELETE_TODO_MUTATION = gql`
+  mutation DeleteTodo($id: ID!) {
+    deleteTodo(where: { id: $id }) {
+      id
+    }
+  }
+`;
+
+type TodoProps = {
+  id: string;
+  nome: string;
+  descricao: string;
+  status_todo: "done" | "undone";
+};
+
 export const Home = () => {
+  const { data } = useQuery<{ todos: TodoProps[] }>(GET_TODOS_QUERY);
+
+  const todos = data?.todos;
+  console.log(todos);
+
   return <div>Home</div>;
 };
